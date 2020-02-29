bbcd8c56c1392d20fab9ce9759be3d7fc8cfb40e
Deni Setiawan <deni.setiawan@live.com>
2020-03-01 01:27:14 +0700

    Activating your application

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,12 +29,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.admin', 'django.contrib.auth',
+    'django.contrib.contenttypes', 'django.contrib.sessions',
+    'django.contrib.messages', 'django.contrib.staticfiles',
+    'blog.apps.BlogConfig'
 ]
 
 MIDDLEWARE = [
